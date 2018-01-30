f28885441e1880d28edf8cf9a99ec4abeb81a195
linjiangl <8257796@qq.com>
2018-01-30 17:57:01 +0800

    admin

---
+++ src/admin/src/views/layout/components/Navbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div style="border-bottom: solid 1px #e6e6e6">
+
+    <el-menu class="navbar" mode="horizontal">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-menu-item index="2">处理中心</el-menu-item>
+
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+          <i class="el-icon-caret-bottom"></i>
+        </div>
+        <el-dropdown-menu class="user-dropdown" slot="dropdown">
+          <router-link class="inlineBlock" to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+          <el-dropdown-item divided>
+            <span @click="logout" style="display:block;">LogOut</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-menu>
+
+    <breadcrumb style="line-height: 22px; margin-left: 20px;"></breadcrumb>
+  </div>
+
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import Breadcrumb from '@/components/Breadcrumb'
+
+  export default {
+    components: {
+      Breadcrumb
+    },
+    computed: {
+      ...mapGetters([
+        'avatar'
+      ])
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch('LogOut').then(() => {
+          location.reload() // 为了重新实例化vue-router对象 避免bug
+        })
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .navbar {
+    height: 50px;
+    line-height: 50px;
+    border-radius: 0px !important;
+    .screenfull {
+      position: absolute;
+      right: 90px;
+      top: 16px;
+      color: red;
+    }
+    .avatar-container {
+      height: 50px;
+      display: inline-block;
+      position: absolute;
+      right: 35px;
+      .avatar-wrapper {
+        cursor: pointer;
+        margin-top: 5px;
+        position: relative;
+        .user-avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+        .el-icon-caret-bottom {
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+</style>
+
